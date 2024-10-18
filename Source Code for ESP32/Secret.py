cff3651ce609b7964043ca09585f2ebd56f8ce13
AI-Intern-3 <interndevops3@gmail.com>
2024-10-18 14:29:54 +0530

    Create Secret.py

---
+++ Source Code for ESP32/Secret.py
@@ -0,0 +1,12 @@
+# secrets.py
+
+WIFI_SSID = "your_wifi_ssid"
+WIFI_PASSWORD = "your_wifi_password"
+
+AWS_ENDPOINT = "your_aws_endpoint.iot.us-west-2.amazonaws.com"
+AWS_CERT = "-----BEGIN CERTIFICATE-----\nYOUR_CERTIFICATE_CONTENT\n-----END CERTIFICATE-----\n"
+AWS_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nYOUR_PRIVATE_KEY_CONTENT\n-----END PRIVATE KEY-----\n"
+AWS_ROOT_CA = "-----BEGIN CERTIFICATE-----\nYOUR_ROOT_CA_CONTENT\n-----END CERTIFICATE-----\n"
+
+SENSOR_TOPIC = "esp32/sensordata"
+CONTROL_TOPIC = "home/automation/control"
